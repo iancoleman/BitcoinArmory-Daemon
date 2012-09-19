002ab1fa3e993cee9cb77f438bc22e4d99f26ab1
Ian Coleman <coleman.ian@gmail.com>
2012-09-19 11:14:40 +1000

    Added formatting for transaction watcher.

--- armory-daemon.py
+++ armory-daemon.py
@@ -46,6 +46,11 @@ class Wallet_Json_Rpc_Server(jsonrpc.JSONRPC):
             raise ValueError('Cannot create transactions when offline')
         return self.create_unsigned_transaction(bitcoinaddress, amount)
 
+    def jsonrpc_listtransactions(self, p_count=10, p_from=0):
+        txs = self.wallet.getTxLedger()
+        for x in txs:
+            print x.pprint()
+
         
     # https://bitcointalk.org/index.php?topic=92496.msg1126310#msg1126310
     def create_unsigned_transaction(self, bitcoinaddress_str, amount_to_send_btc):
@@ -89,10 +94,10 @@ class Armory_Daemon():
             BDM_LoadBlockchainFile()
 
         print "Reading wallet file"
-        wallet = self.find_wallet()
+        self.wallet = self.find_wallet()
         
         print "Initialising server"
-        reactor.listenTCP(RPC_PORT, server.Site(Wallet_Json_Rpc_Server(wallet)))
+        reactor.listenTCP(RPC_PORT, server.Site(Wallet_Json_Rpc_Server(self.wallet)))
 
         self.NetworkingFactory = ArmoryClientFactory( \
                                 func_loseConnect=self.showOfflineMsg, \
@@ -110,8 +115,85 @@ class Armory_Daemon():
     def newTxFunc(self, pytxObj):
         # Cut down version from ArmoryQt.py
         TheBDM.addNewZeroConfTx(pytxObj.serialize(), long(RightNow()), True)
-        print "New incoming TX"
-        TheBDM.rescanWalletZeroConf(wallet.cppWallet)
+        TheBDM.rescanWalletZeroConf(self.wallet.cppWallet)
+
+        # TODO set up a 'subscribe' feature so these notifications can be
+        # pushed out to interested parties.
+
+        # From here down is display purposes only, copied from ArmoryQt.py
+        message = "New TX"
+        le = self.wallet.cppWallet.calcLedgerEntryForTxStr(pytxObj.serialize())
+        if not le.isSentToSelf():
+            txref = TheBDM.getTxByHash(le.getTxHash())
+            nOut = txref.getNumTxOut()
+            recips = [txref.getTxOut(i).getRecipientAddr() for i in range(nOut)]
+            values = [txref.getTxOut(i).getValue()         for i in range(nOut)]
+            idxMine  = filter(lambda i:     self.wallet.hasAddr(recips[i]), range(nOut))
+            idxOther = filter(lambda i: not self.wallet.hasAddr(recips[i]), range(nOut))
+            mine  = [(recips[i],values[i]) for i in idxMine]
+            other = [(recips[i],values[i]) for i in idxOther]
+
+            # Collected everything we need to display, now construct it and do it
+            if le.getValue()>0:
+               # Received!
+               message = 'Bitcoins Received!'
+               totalStr = coin2str( sum([mine[i][1] for i in range(len(mine))]), maxZeros=1)
+               message += '\nAmount: \t%s BTC' % totalStr.strip()
+               if len(mine)==1:
+                  message += '\nAddress:\t%s' % hash160_to_addrStr(mine[0][0])
+                  addrComment = self.wallet.getComment(mine[0][0])
+                  #if addrComment:
+                     #message += '\n%s...' % addrComment[:24]
+               else:
+                  message += '\n<Received with Multiple Addresses>'
+            elif le.getValue()<0:
+               # Sent!
+               message = 'Bitcoins Sent!'
+               totalStr = coin2str( sum([other[i][1] for i in range(len(other))]), maxZeros=1)
+               message += '\nAmount: \t%s BTC' % totalStr.strip()
+               if len(other)==1:
+                  message += 'Sent To:\t%s' % hash160_to_addrStr(other[0][0])
+                  addrComment = self.wallet.getComment(other[0][0])
+                  #if addrComment:
+                     #message += '\n%s...' % addrComment[:24]
+               else:
+                  dispLines.append('<Sent to Multiple Addresses>')
+        else:
+            amt = self.determineSentToSelfAmt(le, self.wallet)[0]
+            message = 'Wallet "%s" just sent %s BTC to itself!' % \
+               (self.wallet.labelName, coin2str(amt,maxZeros=1).strip())
+               
+        print message
+
+    def determineSentToSelfAmt(self, le, wlt):
+      """
+      NOTE:  this method works ONLY because we always generate a new address
+             whenever creating a change-output, which means it must have a
+             higher chainIndex than all other addresses.  If you did something
+             creative with this tx, this may not actually work.
+      """
+      amt = 0
+      if TheBDM.isInitialized() and le.isSentToSelf():
+         txref = TheBDM.getTxByHash(le.getTxHash())
+         if not txref.isInitialized():
+            return (0, 0)
+         if txref.getNumTxOut()==1:
+            return (txref.getTxOut(0).getValue(), -1)
+         maxChainIndex = -5
+         txOutChangeVal = 0
+         txOutIndex = -1
+         valSum = 0
+         for i in range(txref.getNumTxOut()):
+            valSum += txref.getTxOut(i).getValue()
+            addr160 = txref.getTxOut(i).getRecipientAddr()
+            addr    = wlt.getAddrByHash160(addr160)
+            if addr and addr.chainIndex > maxChainIndex:
+               maxChainIndex = addr.chainIndex
+               txOutChangeVal = txref.getTxOut(i).getValue()
+               txOutIndex = i
+
+         amt = valSum - txOutChangeVal
+      return (amt, txOutIndex)
 
     def showOfflineMsg(self):
         print "Offline - not tracking blockchain"
